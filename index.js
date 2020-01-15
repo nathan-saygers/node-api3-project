dc879c413997c0ccebd623c2a9e9b8ab8a86aadb
nathan-saygers <nathan.saygers@gmail.com>
2020-01-15 12:47:49 -0800

    initial commit before writin CRUD

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+
+const server = require('./data/server');
+// add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
+
+server.listen(4000, () => {
+  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+});
