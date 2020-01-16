9e8f9bce781cb9012349fbd6ffe92a593306186c
nathan-saygers <nathan.saygers@gmail.com>
2020-01-16 12:29:58 -0800

    updates knex, helmet, index.js port

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ server.use('/api/user', userRouter);
 
 const port = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
